11e6298508b14d10005b54327c644983719263ae
krishan041 <krishandara04@gmail.com>
2025-01-20 21:22:57 +0530

    Create get_session.py

---
+++ get_session.py
@@ -0,0 +1,8 @@
+from pyrogram import Client, filters, types, enums
+api_id = "27536109"
+api_hash = "b84d7d4dfa33904d36b85e1ead16bd63" 
+bot_token = "7157802362:AAF0Y5u8XyXNCmY9chpekN6LbVgj2U0ZAIY"
+
+app_user = Client("app_user", api_id=api_id, api_hash=api_hash, phone_number=phone_number)
+
+app_user.run()
