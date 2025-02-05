adeb53d156b4b9647d75f2ac1c7f7b99bf3e8d8d
krishan041 <krishandara04@gmail.com>
2025-02-05 20:13:18 +0530

    Update get_session.py

--- get_session.py
+++ get_session.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters, types, enums
 api_id = "27536109"
 api_hash = "b84d7d4dfa33904d36b85e1ead16bd63" 
-bot_token = ""
+bot_token = "8038725964:AAEkw01YH9JJ_soMDKAyqNSQz9q3JhX_yt0"
 
 app_user = Client("app_user", api_id=api_id, api_hash=api_hash, phone_number=phone_number)
 
