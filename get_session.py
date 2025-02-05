6a9bc07ed2bfd69314f15900dd79eb70a92e1d44
manishadara04 <manishadara04@gmail.com>
2025-02-06 01:34:30 +0530

    Update get_session.py

--- get_session.py
+++ get_session.py
@@ -3,6 +3,3 @@ api_id = "27536109"
 api_hash = "b84d7d4dfa33904d36b85e1ead16bd63" 
 bot_token = "8038725964:AAEkw01YH9JJ_soMDKAyqNSQz9q3JhX_yt0"
 
-app_user = Client("app_user", api_id=27536109, api_hash=b84d7d4dfa33904d36b85e1ead16bd63, bot_token=8038725964:AAEkw01YH9JJ_soMDKAyqNSQz9q3JhX_yt0)
-
-app_user.run()
